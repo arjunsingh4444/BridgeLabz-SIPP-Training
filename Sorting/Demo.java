32d39447aebff0bdd398a80fa031d1d14158c876
Arjun Singh <arjunvkas@gmail.com>
2025-08-19 10:55:12 +0530

    Delete Sorting directory

--- Sorting/Demo.java
+++
@@ -1,8 +0,0 @@
-// package BridgeLabz_SIPP_Training.Sorting;
-
-// public class Demo {
-//     public static void main(String[] args) {
-//         System.out.println("arjun");
-//     }
-    
-// }
--- Sorting/j.java
+++
@@ -1,9 +0,0 @@
-// package BridgeLabz_SIPP_Training.Sorting;
-
-// public class j{
-//     public static void main(String[] args) {
-//         System.out.println("arjun");
-//     }
-    
-// }
-
